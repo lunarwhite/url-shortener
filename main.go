9d311e10d3602fff69c0dff917443c9a7e64f419
lunarwhite <dwcn22@outlook.com>
2021-11-01 15:00:13 +0800

    implement in endpoints

--- main.go
+++ main.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lunarwhite/url-shortener/handler"
+	"github.com/lunarwhite/url-shortener/store"
 )
 
 func main() {
-	// fmt.Printf("Hello Go URL Shortener!")
-
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener!",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
+
+	//
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
