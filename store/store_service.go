243ab97e343464ec73622864e0306c3d026282e3
lunarwhite <dwcn22@outlook.com>
2021-11-01 13:43:54 +0800

    create storage layer

---
+++ store/store_service.go
@@ -0,0 +1,58 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Define the struct wrapper around raw Redis client
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+// Top level declarations for the storeService and Redis context
+var (
+	storeService = &StorageService{}
+	ctx          = context.Background()
+)
+
+// Note that in a real world usage, the cache duration shouldn't have an expiration time
+const CacheDuration = 6 * time.Hour
+
+// Initializing the store service and return a store pointer
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
+
+// Save the mapping between the originalUrl and the generated shortUrl url
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration)
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+// Retrieve the long url and think about redirect
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return result
+}
